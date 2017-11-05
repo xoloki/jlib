45dacde58f6e9b21271b863a8e2bb8d447d08622
Joey Yandle <xoloki@gmail.com>
2017-11-05 01:38:15 -0800

    add libjson to configure; move json wrappers from mrx

---
+++ jlib/util/json.cc
@@ -0,0 +1,231 @@
+/* -*- mode: C++ c-basic-offset: 4  -*-
+ * 
+ * Copyright (c) 2010 Joe Yandle <xoloki@gmail.com>
+ * 
+ */
+
+#include <json/json.h>
+
+#include "json.hh"
+
+#include <stdexcept>
+
+namespace mrx {
+    namespace json {
+
+        proxy::proxy(json_object* obj) 
+            : m_obj(obj)
+        {}
+        
+        proxy::operator std::string() {
+            if(!json_object_is_type(m_obj, json_type_string))
+                throw type_mismatch();
+
+            return json_object_get_string(m_obj);
+        }
+
+        proxy::operator std::size_t() {
+            if(!json_object_is_type(m_obj, json_type_int))
+                throw type_mismatch();
+
+            return json_object_get_int(m_obj);
+        }
+
+        proxy::operator int() {
+            if(!json_object_is_type(m_obj, json_type_int))
+                throw type_mismatch();
+
+            return json_object_get_int(m_obj);
+        }
+
+
+        proxy::operator bool() {
+            return (m_obj != 0);
+        }
+
+        proxy::operator unsigned int() {
+            if(!json_object_is_type(m_obj, json_type_int))
+                throw type_mismatch();
+
+            return json_object_get_int(m_obj);
+        }
+
+        proxy::operator double() {
+            if(!json_object_is_type(m_obj, json_type_double))
+                throw type_mismatch();
+
+            return json_object_get_double(m_obj);
+        }
+
+        proxy::operator long double() {
+            if(!json_object_is_type(m_obj, json_type_double))
+                throw type_mismatch();
+
+            return json_object_get_double(m_obj);
+        }
+
+        object::ptr object::create() { 
+            return ptr(new object());
+        }
+
+        object::ptr object::create(std::string data) { 
+            return ptr(new object(data));
+        }
+
+        object::object() 
+            : m_obj(json_object_new_object()),
+              m_put(true)
+        {}
+        
+        object::object(std::string data) 
+            : m_obj(json_tokener_parse(data.data())),
+              m_put(true)
+        {
+            if(is_error(m_obj))
+                throw std::runtime_error("data did not parse to JSON: '" + data + "'");
+        }
+
+        object::object(json_object* obj, bool put) 
+            : m_obj(obj),
+              m_put(put)
+        {
+        }
+        
+        object::~object() { 
+            if(m_put)
+                json_object_put(m_obj); 
+        }
+
+        proxy object::get(std::string key) const {
+            return proxy(json_object_object_get(m_obj, key.data()));
+        }
+
+        proxy object::get(std::size_t idx) const {
+            return proxy(json_object_array_get_idx(m_obj, idx));
+        }
+
+        void object::add(std::string key, std::string val) {
+            json_object_object_add(m_obj, key.data(), json_object_new_string(const_cast<char*>(val.data())));
+        }
+
+        void object::add(std::string key, int val) {
+            json_object_object_add(m_obj, key.data(), json_object_new_int(val));
+        }
+
+        void object::add(std::string key, unsigned int val) {
+            add(key, static_cast<int>(val));
+        }
+
+        void object::add(std::string key, double val) {
+            json_object_object_add(m_obj, key.data(), json_object_new_double(val));
+        }
+
+        void object::add(std::string key, long double val) {
+            add(key, static_cast<double>(val));
+        }
+
+        void object::add(std::string key, object::ptr val) {
+            json_object_object_add(m_obj, key.data(), val->obj());
+            val->m_put = false;
+        }
+
+        void object::add(std::string key, object::arrayptr val) {
+            json_object_object_add(m_obj, key.data(), val->obj());
+            val->m_put = false;
+        }
+
+        std::string object::str() const {
+            std::string ret(json_object_to_json_string(m_obj));
+
+            return ret;
+        }
+
+        json_object* object::obj() {
+            return m_obj;
+        }
+        
+        object::ptr object::obj(std::string key) {
+            json_object* o = json_object_object_get(m_obj, key.data());
+            return ptr(new object(o, false));
+        }
+
+        object::ptr object::obj(unsigned int x) {
+            json_object* o = json_object_array_get_idx(m_obj, x);
+            return ptr(new object(o, false));
+        }
+
+        std::size_t object::size() const {
+            return json_object_array_length(m_obj);
+        }
+
+        array::ptr array::create() { 
+            return ptr(new array());
+        }
+
+        array::ptr array::create(std::string data) { 
+            return ptr(new array(data));
+        }
+
+        array::array() 
+            : m_obj(json_object_new_array()),
+              m_put(true)
+        {}
+        
+        array::array(std::string data) 
+            : m_obj(json_tokener_parse(data.data())),
+              m_put(true)
+        {
+            if(is_error(m_obj))
+                throw std::runtime_error("data did not parse to JSON: '" + data + "'");
+        }
+        
+        array::~array() { 
+            if(m_put)
+                json_object_put(m_obj); 
+        }
+
+        void array::add(std::string val) {
+            json_object_array_add(m_obj, json_object_new_string(const_cast<char*>(val.data())));
+        }
+
+        void array::add(int val) {
+            json_object_array_add(m_obj, json_object_new_int(val));
+        }
+
+        void array::add(unsigned int val) {
+            add(static_cast<int>(val));
+        }
+
+        void array::add(double val) {
+            json_object_array_add(m_obj, json_object_new_double(val));
+        }
+
+        void array::add(long double val) {
+            add(static_cast<double>(val));
+        }
+
+        void array::add(object::ptr val) {
+            json_object_array_add(m_obj, val->obj());
+            val->m_put = false;
+        }
+
+        proxy array::get(unsigned int x) const {
+            return proxy(json_object_array_get_idx(m_obj, x));
+        }
+
+        std::string array::str() const {
+            std::string ret(json_object_to_json_string(m_obj));
+
+            return ret;
+        }
+
+        int array::size() const {
+            return json_object_array_length(m_obj);
+        }
+
+        json_object* array::obj() {
+            return m_obj;
+        }
+
+    }
+}
---
+++ jlib/util/json.hh
@@ -0,0 +1,123 @@
+/* -*- mode: C++ c-basic-offset: 4  -*-
+ * 
+ * Copyright (c) 2010 Joe Yandle <xoloki@gmail.com>
+ * 
+ */
+
+#ifndef JLIB_JSON_HH
+#define JLIB_JSON_HH
+
+#include <string>
+#include <exception>
+
+#include <memory>
+
+struct json_object;
+
+namespace jlib {
+namespace util {
+namespace json {
+
+class array;
+
+class proxy {
+public:
+    class type_mismatch : public std::exception {};
+    
+    proxy(json_object* obj);
+    
+    operator std::string();
+    operator std::size_t();
+    operator int();
+    operator unsigned int();
+    operator double();
+    operator long double();
+    operator bool();
+    
+protected:
+    json_object* m_obj;
+};
+    
+class object {
+public:
+    typedef boost::shared_ptr<object> ptr;
+    typedef boost::shared_ptr<array> arrayptr;
+    
+    static ptr create();
+    static ptr create(std::string data);
+    
+    virtual ~object();
+    
+    void add(std::string key, std::string val);
+    void add(std::string key, int val);
+    void add(std::string key, unsigned int val);
+    void add(std::string key, double val);
+    void add(std::string key, long double val);
+    void add(std::string key, ptr val);
+    void add(std::string key, arrayptr val);
+    
+    proxy get(std::string key) const;
+    proxy get(std::size_t idx) const;
+    
+    ptr obj(std::string key);
+    ptr obj(unsigned int x);
+    
+    std::string str() const;
+    
+    json_object* obj();
+    
+    std::size_t size() const;
+    
+    friend class array;
+    
+private:
+    object();
+    object(json_object* obj, bool put);
+    object(std::string data);
+    object(const object&);
+    
+    json_object* m_obj;
+    bool m_put;
+};
+    
+class array {
+public:
+    typedef boost::shared_ptr<array> ptr;
+    
+    static ptr create();
+    static ptr create(std::string data);
+    
+    virtual ~array();
+    
+    void add(std::string val);
+    void add(int val);
+    void add(unsigned int val);
+    void add(double val);
+    void add(long double val);
+    void add(object::ptr val);
+    
+    proxy get(unsigned int x) const;
+    
+    std::string str() const;
+    
+    int size() const;
+    
+    json_object* obj();
+    
+    friend class object;
+    
+private:
+    array();
+    array(std::string data);
+    array(const object&);
+    
+    json_object* m_obj;
+    bool m_put;
+};
+
+}
+}
+}
+
+#endif
+
