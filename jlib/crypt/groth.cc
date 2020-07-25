7977db17ab947cfc794711009f0051e391f89942
Joey Yandle <dragon@dancingdragon.net>
2020-07-25 04:06:40 -0700

    group terms; use += to calculate the products

--- jlib/crypt/groth.cc
+++ jlib/crypt/groth.cc
@@ -136,7 +136,7 @@ ZeroProof prove(const std::vector<curve::Commitment>& c, std::size_t l, const cu
         curve::Point c_d_k = proof.c[0] * p_x[0][j] + c_0_rho_0;
         for(std::size_t i = 1; i < N; i++) {
             curve::Point c_0_rho_k = curve::Commitment(0, rho[j]);
-            c_d_k += proof.c[i] * p_x[i][j] + c_0_rho_k;
+            c_d_k += (proof.c[i] * p_x[i][j] + c_0_rho_k);
         }
 
         proof.c_d.push_back(c_d_k);
@@ -172,7 +172,7 @@ ZeroProof prove(const std::vector<curve::Commitment>& c, std::size_t l, const cu
 
     proof.z_d = r * x^n;
     for(int k = 0; k < n; k++) {
-        proof.z_d -= rho[k] * x^k;
+        proof.z_d -= (rho[k] * x^k);
     }
     
     return proof;
@@ -241,10 +241,10 @@ bool verify(const ZeroProof& proof) {
             P_f_j_i_j *= f_j_i_j;
         }
         
-        P_c_i_f_j_i_j = proof.c[i] * P_f_j_i_j;
+        P_c_i_f_j_i_j += (proof.c[i] * P_f_j_i_j);
     }
 
-    if(P_c_i_f_j_i_j + P_c_d_k_x_k != c0zd) {
+    if((P_c_i_f_j_i_j + P_c_d_k_x_k) != c0zd) {
         std::cerr << "groth zeroproof failed to verify because product part failed" << std::endl;
         return false;
     }
